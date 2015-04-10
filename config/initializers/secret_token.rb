1a02087ba3e124202dd9747a5ded5573ab864d08
Stanley Calixte <scalixte@mdsol.com>
2015-04-10 16:44:36 -0400

    make project a rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JanusWithProtractor::Application.config.secret_token = '268e8a42093aff7f95968063c9951a5e622d8bf45e2f25efbf163e6527388fa03d98c73e33b7ba48767810a59e79b2e921aae8300b6af3cbc09e7012ba011b7a'
