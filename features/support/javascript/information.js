b9a6715dca3708ee49223949ce27932ca0b0f0ef
Stanley Calixte <scalixte@mdsol.com>
2015-04-28 15:15:25 -0400

    methods that gather all config data files

--- features/support/javascript/information.js
+++ features/support/javascript/information.js
@@ -6,27 +6,33 @@
 var yaml = require('js-yaml');
 var fs = require('fs');
 var glob = require("glob");
-var Information = module.exports = function () {
+
+var Information = function () {
+    function selection() {
+        var selections = {};
+        var files = glob.sync("features/support/locators/**/*.yml", 'matchBase');
+        var concat = (function (json1, json2) {
+            var json3 = {};
+            for (var attribute_name in json1) {
+                json3[attribute_name] = json1[attribute_name];
+            }
+            for (var attribute_name in json2) {
+                json3[attribute_name] = json2[attribute_name];
+            }
+            return json3;
+        });
+        for (var i in files) {
+            selections = concat(selections, yaml.safeLoad(fs.readFileSync(files[i], 'utf8')));
+        }
+        return selections;
+    }
+
     try {
         this.config = yaml.safeLoad(fs.readFileSync('features/support/config.yml', 'utf8'));
-        this.selector = {}
-        glob("features/support/locators/**/*.yml", 'matchBase', function (er, files) {
-            var concat = (function (json1, json2) {
-                var json3 = {};
-                for (var attribute_name in json1) {
-                    json3[attribute_name] = json1[attribute_name];
-                }
-                for (var attribute_name in json2) {
-                    json3[attribute_name] = json2[attribute_name];
-                }
-                return json3;
-            });
-            for (var i in files) {
-                this.selector = concat(this.selector, yaml.safeLoad(fs.readFileSync(files[i], 'utf8')));
-            }
-            console.log(this.selector['Imedidata']['Sign In']['username']['label']);
-        });
+        this.selector = this.selector || selection();
     } catch (e) {
         console.log(e);
     }
 };
+
+module.exports = new Information();
