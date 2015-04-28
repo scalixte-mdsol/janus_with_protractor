62bdcad2c7965505c344de6d973541c9fa98536c
Stanley Calixte <scalixte@mdsol.com>
2015-04-28 15:05:34 -0400

    create page object model to handle janus client division landing page

--- features/support/pages/janus/landing.rb
+++ features/support/pages/janus/landing.rb
@@ -1,2 +1,11 @@
-class Landing
+require_relative File.join(Rails.root, 'features', 'support', 'page.rb').to_s
+
+module JanusHelper
+  class Landing < Page
+    def self.visit
+      page.visit JanusNavigation::CONFIG_MAPPING['apps']['landing page']
+      new
+    end
+  end
 end
+World(JanusHelper)
