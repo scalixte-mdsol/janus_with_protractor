0357072021a2cd4fd21a8a5575230b6a731bd574
Stanley Calixte <scalixte@mdsol.com>
2015-04-28 15:13:54 -0400

    methods to call seleniourm or protractors as needed

--- features/support/javascript/support.js
+++ features/support/javascript/support.js
@@ -1,12 +1,47 @@
 /**
  * Created by scalixte on 4/27/15.
  */
+var webdriver = webdriver || require('selenium-webdriver');
 var Support = function(){
 };
+
+Support.prototype.ignoreSynchronization=function(sut, status, callback){
+    sut.browser.ignoreSynchronization = status;
+}
 
 Support.prototype.get = function(sut, url, callback){
     sut.browser.get(url).then(function(result) {
         callback(result)
+    });
+};
+
+Support.prototype.driver = function(sut){
+    return sut.browser.driver;
+};
+
+//Support.prototype.change_url = function(sut, new_url, callback){
+//    var currentUrl;
+//    sut.browser.getCurrentUrl().then(function(url){
+//        currentUrl = url;
+//    }).then(then(function(){
+//        sut.browser.wait(function(){
+//
+//        });
+//    });
+//    sut.browser.get(url).then(function(result) {
+//        callback(result)
+//    });
+//};
+
+Support.prototype.findByName = function(sut, item, callback){
+    sut.browser.driver.findElement(webdriver.By.name(item)).then(function(result) {
+        callback(result);
+    });
+};
+
+Support.prototype.findByCSS = function(sut, item, callback){
+    sut.browser.driver.findElement(webdriver.By.css(item)).then(function(result) {
+        callback(result);
     });
 };
 
