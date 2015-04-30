c43ef2657358674de4533de24b9f157f426ceb5a
Stanley Calixte <scalixte@mdsol.com>
2015-04-30 15:15:41 -0400

    add support for logout method

--- features/support/pages/imedidata/imedidata.rb
+++ features/support/pages/imedidata/imedidata.rb
@@ -1,14 +1,18 @@
 require_relative File.join(Rails.root, 'features', 'support', 'page.rb').to_s
+require 'memoize'
 
 module JanusHelper
   class Imedidata < Page
+    # include Memoize
     def self.visit
       page.visit JanusNavigation::CONFIG_MAPPING['apps']['login']
       new
+      # self.memoize(:mapper)
     end
 
     def login
       selector, config = mapper
+      selector = selector['Sign In']
 
       fill_in selector['username']['label'], with: config['username']
       fill_in selector['password']['label'], with: config['password']
@@ -16,13 +20,21 @@ module JanusHelper
     end
 
     def logout
+      selector, config = mapper
+      selector = selector['Header']
 
+      within selector['section']['panel'] do
+        click_link config['username']
+        page.find(selector['logout']['link'],visible: true).click
+      end
     end
 
     private
 
     def mapper
-      return JanusNavigation::SELECTOR_MAPPING['Imedidata']['Sign In'], JanusNavigation::CONFIG_MAPPING['apps']
+      @selector ||= JanusNavigation::SELECTOR_MAPPING['Imedidata']
+      @config ||= JanusNavigation::CONFIG_MAPPING['apps']
+      return @selector, @config
     end
   end
 end
